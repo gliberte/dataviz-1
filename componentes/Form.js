bbc7557a238b254334223e59d3ab84151d77ce27
Luis Solano <gliberte@yahoo.com>
2017-08-24 09:04:37 -0500

    ok

--- componentes/Form.js
+++ componentes/Form.js
@@ -10,18 +10,21 @@ export default class Form extends React.Component{
             closeOnSelect:false
         })
     }
+    onChange = (e)=>{
+        console.log(e.target)
+    }
     render(){
         return(
             <div className="row">
             <div className={`form-container ${this.props.visible ? "form-container-visible":""} col s4 offset-s4 z-depth-2 light-green lighten-2`}>
                 <form>
                     <div className="input-field">
-                        <input id="fecha" name="fecha" type="text" className="datepicker"/>
-                        <label htmlFor="fecha">Fecha del Registro</label>
+                        <input id="fecha" name="fecha" type="text" className="datepicker" onChange={this.onChange}/>
+                        <label htmlFor="fecha" className="teal-text">Fecha del Registro</label>
                     </div>
                     <div className="input-field">
-                        <input id="peso" name="peso" type="text"/>
-                        <label htmlFor="peso">Peso</label>
+                        <input id="peso" name="peso" type="text" onChange={this.onChange}/>
+                        <label htmlFor="peso" className="teal-text">Peso</label>
                     </div>
                     <div className="action">
                         <a className="waves-effect-waves light btn" onClick={this.props.onCerrar}>Cerrar</a>
